10e3620d4a0838354efa03f1d6a4802238728e45
Auyer <rafael@rcpassos.me>
2023-01-20 19:19:09 -0300

    improves my self description

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -16,6 +16,35 @@
 </container>
 <br />
 <container class="flex flex-col flex-grow w-full mx-auto max-w-4xl">
+  <h1>Rafael, the human being</h1>
+  <p>
+    I am a curious person and an explorer. Understanding things is one of my biggest motivators. It
+    has to be a superficial understanding in areas out of my scope, but I like to get a fuller
+    picture when I can.
+  </p>
+
+  <p>
+    When it comes to technology, I am always testing new things, new languages, software, and
+    projects. I do this looking for better solutions to real solutions, or just to get to know new
+    things that could be useful someday.
+  </p>
+
+  <p>
+    I love to travel (that's me exploring the world, I guess). I believe seeing different places,
+    cultures and people is very enriching to me as an individual.
+  </p>
+
+  <br />
+</container>
+<container class="flex flex-col flex-grow w-full mx-auto max-w-4xl">
+  <h1>Rafael, the professional</h1>
+  <p>
+    I have a questioning mind guided by reason. I always aim to establish trust in my workspace, so
+    I can propose and discuss changes. I try to improve the quality of the projects I work on, both
+    on a technical (engineering) and a product approach.
+  </p>
+
+  <p>I gravitate toward being in the planning and discovery of products and solutions.</p>
   <p>
     I am a Computer Scientist with a Bachelor’s Degree from IFB - <i
       >Instituto Federal de Brasília</i
@@ -32,11 +61,11 @@
     personal interests, I specialized in Data Engineering.
   </p>
 
-  <p>I formed and led a six members squad with Data analysts and Data engineers.</p>
-
-  <p>In this team, I was the technical leader and guided our solutions.</p>
-
-  <p>With them, I created and evolved what became our current data platform.</p>
+  <p>
+    I formed and led a six members squad with Data analysts and Data engineers. In this team, I was
+    the technical leader and guided our solutions. With them, I created and evolved what became our
+    current data platform.
+  </p>
 
   <p>
     Since then, we have gone through a downsizing. I no longer have a team to manage. I am still the
@@ -49,7 +78,9 @@
 
 <style lang="scss">
   $w-s: 750px;
-
+  p {
+    line-height: 1.8em;
+  }
   .hero {
     position: relative;
     overflow: hidden;
