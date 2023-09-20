da906a4d109e1346ceb02bb6d4cecdf65dd97567
auyer <rafael@rcpassos.me>
2023-09-20 10:03:28 -0300

    fixes wal, reconection logic, simple exp backoff

--- src/lib/store/wal.js
+++ src/lib/store/wal.js
@@ -2,20 +2,41 @@ import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
 export const messageStore = writable();
 // const API_ENDPOINT = 'ws://localhost:8080';
-const API_ENDPOINT = 'ws://kv.rcpassos.me';
+const API_ENDPOINT = 'ws://kv2.rcpassos.me';
 
-export const openConnection = () => {
+const MAX_ATTEMPTS = 3;
+
+export function openConnection(attempt = 0) {
 	if (browser) {
 		messageStore.set('Connecting to WAL endpoint');
-		const socket = new WebSocket(API_ENDPOINT + '/wal');
-		// Connection opened
-		socket.addEventListener('open', function () {
-			messageStore.set('Connected');
-		});
-		// Listen for messages
-		socket.addEventListener('message', function (event) {
-			// console.log('Message from server ', event.data)
-			messageStore.set(event.data);
-		});
+    const ws = new WebSocket(API_ENDPOINT + '/wal');
+
+    ws.onerror = function (err) {
+      console.error('Socket encountered error: ', err, 'Closing socket');
+      ws.close();
+    };
+
+    ws.onclose = function (e) {
+      // simple exponential backoff
+      const timeout =  1000 * (attempt + 1 ** 2)
+      console.log(`Socket is closed. Reconnect will be attempted in ${timeout/1000} second(s).`, e.reason);
+      messageStore.set('Reconnecting');
+      if (attempt > MAX_ATTEMPTS) {
+        console.log("Max reconnection attempts reached")
+        messageStore.set('Max reconnection attempts reached');
+        return;
+      }
+      setTimeout(function () {
+        openConnection(attempt +1);
+      }, timeout);
+    };
+    
+    ws.onopen = function () {
+      messageStore.set('Connected');
+    };
+
+    ws.onmessage = function (event) {
+      messageStore.set(event.data);
+    };
 	}
 };
