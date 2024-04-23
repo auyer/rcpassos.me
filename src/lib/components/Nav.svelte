d0e02382c6e5b3b1b9d7601a3ec01a00a0c31037
auyer <rafael@rcpassos.me>
2024-04-23 08:04:57 -0300

    addd aria-label to menu-button and and svg icon

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -81,8 +81,8 @@
 		</svelte:fragment>
 		<svelte:fragment slot="trail">
 			<div class="block lg:hidden">
-				<button class="flex items-center px-3 py-2 rounded" on:click={clickHandler}>
-					<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+				<button class="flex items-center px-3 py-2 rounded" aria-label="expand head menu" on:click={clickHandler}>
+					<svg aria-label="expand head links icon" viewBox="0 0 100 80" class="fill-token w-4 h-4">
 						<rect width="100" height="20" />
 						<rect y="30" width="100" height="20" />
 						<rect y="60" width="100" height="20" />
