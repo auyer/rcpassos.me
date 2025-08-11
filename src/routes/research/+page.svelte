40341fe5f6640317da2c295a1846bf6ea41346fe
Rafael Passos <rafael@rcpassos.me>
2025-08-11 00:08:20 -0300

    add research page, update vissoft preprint
    Signed-off-by: Rafael Passos <rafael@rcpassos.me>

---
+++ src/routes/research/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="js">
+	// TODO: generate this dynamically
+	const papers = [
+		{
+			name: 'Linux kernel With DUKS',
+			published: '13th IEEE VISSOFT',
+			year: 2025,
+			url: 'research/linux-kernel-with-DUKS-vissoft-2025'
+		}
+	];
+
+	// Author's ORCID information
+	const orcidId = '0009-0003-3657-5519';
+	const orcidUrl = `https://orcid.org/${orcidId}`;
+</script>
+
+<div class="flex mx-auto lg:max-w-none items-center justify-center">
+	<div class="max-w-2xl w-full rounded-lg shadow-lg p-6 md:p-8 space-y-6">
+		<header class="border-b border-slate-200 pb-4">
+			<h1 class="text-3xl font-bold text-slate-800">Research Preprint Repository</h1>
+			<p class="text-slate-500 mt-1">My academic contributions and research</p>
+		</header>
+		<section>
+			<h3 class="text-xl font-semibold text-slate-700 mb-3">My ORCID:</h3>
+			<p>
+				<a
+					href={orcidUrl}
+					target="_blank"
+					rel="noopener noreferrer"
+					class="inline-flex items-center gap-2 text-gray-600 text-3xl hover:text-green-600 transition-colors group"
+				>
+					<img src="/assets/ORCID_iD.svg" alt="ORCID iD" width="50" height="50" />
+					<span class="font-mono tracking-wide group-hover:underline">{orcidId}</span>
+				</a>
+			</p>
+		</section>
+		<section>
+			<h3 class="text-xl font-semibold text-slate-700 mb-3">Publications</h3>
+			<ul class="space-y-3">
+				{#each papers as paper, i}
+					<div class="card p-4 m-2 space-y-4">
+						<li class="flex items-start gap-3">
+							<span>{paper.year} -</span>
+							<a href={paper.url} class="text-blue-600 hover:text-blue-800 hover:underline">
+								{paper.name}
+							</a>
+							<p>
+								{paper.published}
+							</p>
+						</li>
+					</div>
+				{/each}
+			</ul>
+		</section>
+	</div>
+</div>
---
+++ src/routes/research/linux-kernel-with-DUKS-vissoft-2025/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import pdfUrl from './linux-kernel-with-duks-preprint.pdf?url';
+</script>
+
+<div class="w-full justify-center lg:w-4/6 mx-auto overflow-x-hidden">
+	<article class="prose max-w-none">
+		<header class="flex flex-col"></header>
+		<div class="prose max-w-5xl dark:prose-invert">
+			<p>This article was accepted by the IEEE VISSOFT 2025, Visualization Challenge Track.</p>
+			<p>
+				DUKS an innovative framework that supports multiple visualizations and data analyses
+				previously unsupported for the Linux kernel. Using the Linux kernel mainline as an example,
+				we demonstrate how DUKS could provide valuable insights for understanding the health of the
+				kernel maintainership model. By coupling information from the kernel git trees collected
+				from the Software Heritage repository alongside authorship information shared in mailing
+				lists, we envision DUKS as a cornerstone open-access utility to support analyses on the
+				Linux kernel evolution and maintenance.
+			</p>
+			<embed src={pdfUrl} type="application/pdf" class="w-full" height="1200px" />
+			<p>
+				© 2025 IEEE. Personal use of this material is permitted. Permission from IEEE must be
+				obtained for all other uses, in any current or future media, including
+				reprinting/republishing this material for advertising or promotional purposes, creating new
+				collective works, for resale or redistribution to servers or lists, or reuse of any
+				copyrighted component of this work in other works.
+			</p>
+		</div>
+	</article>
+</div>
