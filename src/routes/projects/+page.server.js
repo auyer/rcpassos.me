44c2111db86f8be850cd63531b32a9b262d9cd9d
Rafael Passos <rafael@rcpassos.me>
2026-04-27 16:46:20 -0300

    update projects page

--- src/routes/projects/+page.server.js
+++ src/routes/projects/+page.server.js
@@ -3,7 +3,8 @@ export async function load() {
 	let projects = [
 		{
 			name: 'Protonup-rs',
-			description: 'A Rust app to Install and Update GE-Proton for Steam, and Wine-GE for Lutris',
+			description:
+				'A Rust app to automate the installation and update of Linux Gaming Compatibility tools, like ProtonGE, Luxtorpeda, Boxtron and others.',
 			link: 'https://github.com/auyer/Protonup-rs',
 			cta: 'github.com/auyer/Protonup-rs'
 		},
@@ -14,13 +15,14 @@ export async function load() {
 			link: 'https://github.com/auyer/steganography',
 			cta: 'github.com/auyer/steganography'
 		},
-		{
-			name: 'MemoryKV',
-			description:
-				"A KV in memory database built in Rust for learning purposes. It streams all changes using websocket. The demo is hosted in a tiny free-tier instance. <br><b>But go check the Demo! It's fun!</b><br>Open it in two tabs, or two devices for a more interesting experience.",
-			link: '/projects/kv',
-			cta: 'Check the live demo here'
-		},
+		// KV is offline. Re add when  back on
+		// {
+		// 	name: 'MemoryKV',
+		// 	description:
+		// 		"A KV in memory database built in Rust for learning purposes. It streams all changes using websocket. The demo is hosted in a tiny free-tier instance. <br><b>But go check the Demo! It's fun!</b><br>Open it in two tabs, or two devices for a more interesting experience.",
+		// 	link: '/projects/kv',
+		// 	cta: 'Check the live demo here'
+		// },
 		{
 			name: 'Linux Patches',
 			description:
@@ -29,11 +31,26 @@ export async function load() {
 			cta: 'Check my patches upstream'
 		},
 		{
-			name: 'Lagoinha-rs',
+			name: 'DUKS - Dashboard for Unified Kernel Statistics',
 			description:
-				'Rust library that retrieve Addresses from the Brazilian Postal Code (CEP) using multiple APIs asynchronously, returning the result from the first one to respond.',
-			link: 'https://github.com/auyer/lagoinha-rs',
-			cta: 'github.com/auyer/lagoinha-rs'
+				'A Proof of concept Analytical platform for the Linux Kernel. Part of the PhD research.',
+			link: 'https://duks.rcpassos.me/',
+			cta: 'duks.rcpassos.me/'
+		},
+		{
+			name: 'MLH-Archiver',
+			description: `Collect and archive locally emails from mailing lists, and transform them into Parquet Datasets.
+					A multipart software, with an extensible Rust Collector and Python transformation actors.
+						Work in progress.
+						Part of the PhD research`,
+			link: 'https://github.com/linux-duks/MLH-archiver',
+			cta: 'github.com/linux-duks/MLH-archiver'
+		},
+		{
+			name: 'PublicInboxStack',
+			description: `Configurable Setup to Mirror and Host the Lore (PublicInbox) Software with Mailing Lists. Part of the PhD research.`,
+			link: 'https://github.com/linux-duks/Public-Inbox-Stack',
+			cta: 'github.com/linux-duks/Public-Inbox-Stack'
 		}
 	];
 
