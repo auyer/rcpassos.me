8e3a73f719e98e4edf0530ccd44c504f07fb1a03
Rafael Passos <rafael@rcpassos.me>
2026-05-14 14:10:38 -0300

    Add explicit CC By Sa license to footer
    Signed-off-by: Rafael Passos <rafael@rcpassos.me>

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,16 +1,17 @@
-<script>
-</script>
-
 <footer class="footer">
-	&copy; {new Date().getFullYear()} Rafael Passos
+	{new Date().getFullYear()} Rafael Passos
+	<a href="https://creativecommons.org/licenses/by-sa/4.0/">
+		<img src="/by-sa.svg" alt="License: CC by-sa 4.0" border="0" width="100" />
+	</a>
 </footer>
 
 <style lang="scss">
 	footer {
 		text-align: center;
-		padding-top: 2rem;
-		padding-right: 1rem;
-		padding-bottom: 1rem;
-		padding-left: 1rem;
+		padding-top: 1.5rem;
+		padding-bottom: 1.5rem;
+		color: var(--terminal-fg2);
+		font-size: 11px;
+		border-top: 1px solid var(--terminal-border);
 	}
 </style>
