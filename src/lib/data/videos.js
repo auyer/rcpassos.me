eed7f8a62925bc0c40a0262a5662ad5bf00a4649
Rafael Passos <rafael@rcpassos.me>
2026-07-10 11:26:58 -0300

    content: add Wiimote Linux Driver video
    Signed-off-by: Rafael Passos <rafael@rcpassos.me>

--- src/lib/data/videos.js
+++ src/lib/data/videos.js
@@ -1,2 +1,14 @@
 export const videos = [
+	{
+		slug: 'wiimote-led-linux-driver',
+		src: '/videos/wiimote-led-linux-driver.mp4',
+		coverImage: '/videos/wiimote-led-linux-driver.jpg',
+		title: 'Wiimote Linux LED Driver',
+		date: '2026-07-10',
+		description: `A feature demo for the Wiimote Linux Driver.
+                    Previously, the Linux Driver for the Wii remotes turned the first LED on every connected controller.
+                    Now, each controller that connects receive an ID, and this is used to turn on a specific LED (from 1 to 4).
+                    The demo video presents 4 controllers connecting in different order, with a dmesg log in the background.
+                    `
+	}
 ];
